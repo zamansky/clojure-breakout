ea407c06009ead4afedaca89a503b0602fd46e4c
Mike Zamansky <zamansky@gmail.com>
2019-09-06 15:53:31 -0400

    copied canvas code from older project - need to modify and get working

--- src/main.cljs
+++ src/main.cljs
@@ -5,6 +5,38 @@
             ))
 
 
+(defn draw-cells [component state]
+  (let [canvas (r/dom-node component)
+        ctx (.getContext canvas "2d")
+        ]
+    (set! (. ctx -fillStyle) (str "rgb(" 255 "," 255"," 255 ")"))
+    (.fillRect ctx 0 0 300 300)
+    
+    (set! (. ctx -fillStyle) (str "rgb(" 0 "," 255"," 0 ")"))
+    (if (:target @state)
+      (let [ [x y] (:target @state)] (.fillRect ctx (* 10 x) (* 10 y) 10 10)))
+    (set! (. ctx -fillStyle) (str "rgb(" 255 "," 0"," 0 ")"))
+    (doseq [ [x y] (:body @state)]
+      (.fillRect ctx (* 10 x) (* 10 y) 10 10)                                 
+      )
+
+    )
+  )
+
+
+(defn canvas [state]
+  (let [state state]
+    (r/create-class
+     {:display-name "canvas"
+      :component-did-mount #(draw-cells % state) 
+      :component-did-update #(draw-cells % state)
+      :reagent-render (fn  []
+                        @state
+                        [:canvas { :id "c" :width 300 :height 300 :style {:border "2px solid green"}}])
+      }
+     )))
+
+
 
 (defn main-component []
   [:div 
